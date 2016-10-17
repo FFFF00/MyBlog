95dae0ddc9a59ce5460d084fb65e5075a2586a58
FFFF00 <ff_ff_00@outlook.com>
2016-10-17 23:56:29 +0800

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('ip_id');
             $table->string('head_img');
-            $table->integer('is_anony');->default(0);
+            $table->integer('is_anony')->default(0);
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
